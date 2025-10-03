9b32a42158837cc3fb08c1a066d9a7467cf8effd
hdyess <henrypdyess@gmail.com>
2025-10-03 13:12:31 -0400

    getters and setters for vehicle class

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+    //attributes
+    private long vehicleId;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private float price;
+
+
+
+
+    //getters setters
+    public long getVehicleId() {
+        return vehicleId;
+    }
+    public void setVehicleId(long newVehicleID) {
+        vehicleId = newVehicleID;
+    }
+
+
+    public String getMakeModel() {
+        return  makeModel;
+    }
+    public void setMakeModel(String newMakeModel) {
+        makeModel = newMakeModel;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String newColor) {
+
+    }
+
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+    public void setOdometerReading(int newOdometerReading) {
+
+    }
+
+
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float newPrice) {
+
+    }
+
+
+
+}
